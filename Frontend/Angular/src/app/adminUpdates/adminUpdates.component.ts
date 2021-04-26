fec99192a6dff407871d73b971009e69cdf82b68
eduardosoy <61882315+eduardosoy@users.noreply.github.com>
2021-04-26 19:09:44 +0200

    adminUpdates added

---
+++ Frontend/Angular/src/app/adminUpdates/adminUpdates.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'adminUpdates',
+  templateUrl: './adminUpdates.component.html'
+})
+export class AdminUpdatesComponent {
+  iconUrl = "assets/plusicon.png"
+}
