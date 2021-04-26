c1a60163a625ebb50115df3301f6b4873da7975c
expiQ479 <61882281+expiQ479@users.noreply.github.com>
2021-04-26 13:14:46 +0200

    (Made by carlos) post service and models

---
+++ Frontend/Angular/src/app/models/post.model.ts
@@ -0,0 +1,12 @@
+import { PostType } from './../enums/PostType';
+export interface Post{
+  id:number;
+  title:string,
+  author:string,
+  creationDate:string,
+  updateDate:string,
+  fromGameID:number,
+  postText:string,
+  postType:PostType,
+  imagePath:string
+}
---
+++ Frontend/Angular/src/app/services/post.service.ts
@@ -0,0 +1,27 @@
+import { Post } from './../interfaces/post.model';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+const BASE_URL_POSTS: string = 'api/posts/';
+@Injectable({providedIn:'root'})
+export class PostService{
+  constructor(private httpClient: HttpClient) { }
+    getAllPosts():Observable<Post[]>{
+        return this.httpClient.get(BASE_URL_POSTS).pipe(
+          catchError(error=> this.handleError(error))
+        ) as Observable<Post[]>;
+    }
+
+    getPostByID(id: number): Observable<Post>{
+        return this.httpClient.get(BASE_URL_POSTS + id).pipe(
+          catchError(error => this.handleError(error))
+        ) as Observable<Post>;
+    }
+
+    private handleError(error: any) {
+		console.error(error);
+		return Observable.throw('Server error (' + error.status + '): ' + error.text())
+	}
+}
