c48f259703024a3e9a2c98131c4b435d9606a512
expiQ479 <61882281+expiQ479@users.noreply.github.com>
2021-04-27 10:30:42 +0200

    login in angular update

--- Frontend/Angular/src/app/header/header.component.ts
+++ Frontend/Angular/src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { User } from '../models/user.model';
+import { LoginService } from '../services/login.service';
 import { UserService } from '../services/user.service';
 
 @Component({
@@ -7,9 +9,10 @@ import { UserService } from '../services/user.service';
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  user:User;
   imgUrl = "assets/logo.png"
 
-  constructor(private router: Router){ }
+  constructor(private router: Router, loginService: LoginService, userService: UserService){ }
 
   gotoIndex() {this.router.navigate(['index']);}
   gotoLogin() {this.router.navigate(['login']);}
--- Frontend/Angular/src/app/login/login.component.ts
+++ Frontend/Angular/src/app/login/login.component.ts
@@ -1,9 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'login',
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
-  
+
+  constructor(public loginService: LoginService,private router: Router) { }
+
+  logIn(event: any, user: string, pass: string) {
+
+    event.preventDefault();
+
+    this.loginService.logIn(user, pass);
+
+    this.router.navigate(['index']);
+  }
+
+  logOut() {
+    this.loginService.logOut();
+  }
+
 }
